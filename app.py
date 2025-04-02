72dcde83e2080576b6c23eead5f6b0058099988b
David Patrishkoff <dpatrish@nova.edu>
2025-04-02 13:58:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="Titanic AutoML App", layout="wide")
 
-from shap_interpretability import run_shap_panel
-
-
 # Now safely try to import the SHAP panel
 try:
     from shap_interpretability import run_shap_panel
