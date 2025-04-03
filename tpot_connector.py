51cef69655bedfedf474b0bfa31ba75e138345a8
David Patrishkoff <dpatrish@nova.edu>
2025-04-03 14:13:25 -0400

    Add files via upload

---
+++ tpot_connector.py
@@ -0,0 +1,10 @@
+
+# tpot_connector.py
+
+# This module stores and shares the latest TPOT model and data for use in SHAP and Q&A panels.
+
+latest_tpot_model = None
+latest_X_train = None
+latest_y_train = None
+latest_X_test = None
+latest_y_test = None
