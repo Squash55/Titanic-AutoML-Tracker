cdbcfae14128c523c1f19aa8c626986868cd39b9
David Patrishkoff <dpatrish@nova.edu>
2025-04-04 10:24:52 -0400

    Update tpot_connector.py

--- tpot_connector.py
+++ tpot_connector.py
@@ -1,5 +1,10 @@
 
 # tpot_connector.py
+_tpot_cache = {}
+
+def update_tpot_cache(key, value):
+    _tpot_cache[key] = value
+
 
 # This module stores and shares the latest TPOT model and data for use in SHAP and Q&A panels.
 
